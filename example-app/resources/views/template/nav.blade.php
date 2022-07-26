6159cd0109c9b8deafb71c934d41db81f2bdab2c
orizaar <orizaar@gmail.com>
2022-07-26 11:59:05 +0700

    first commit

---
+++ example-app/resources/views/template/nav.blade.php
@@ -0,0 +1,6 @@
+<nav class="nav bg-dark">
+    <a class="nav-link active" aria-current="page" href="#">kato</a>
+    <a class="nav-link" href="#">Kato</a>
+    <a class="nav-link" href="#">Kato??</a>
+    <a class="nav-link disabled">KATO!!!</a>
+  </nav>
